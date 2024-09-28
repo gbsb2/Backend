163650059d9063d69d8a9b4f6363ed740c2a8f31
20223096 <won10201@kookmin.ac.kr>
2024-09-28 23:11:24 +0900

    commit

--- controller/userInfoController.js
+++ controller/userInfoController.js
@@ -3,11 +3,14 @@ const User = require('../schema/user');
 const mongoose = require('mongoose')
 // 퀴즈 조회
 exports.info = async (req, res) => {
-
+        const {username} = req.body
     try {
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username : username })
         if (user) {
-            res.status(200).json(user);
+            res.status(200).json({
+                name : user.name,
+                password : user.password,
+                email : user.email});
         } else {
             res.status(404).json({ message: "잘못된 요청입니다." });
         }
