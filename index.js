60d7232c33c053163cc7b1bd5165bd5910530233
inwoo <inwooha00@naver.com>
2024-09-28 16:17:39 +0900

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+// router import
+
+// use router
+app.use()
+
+app.listen(3000, () => {
+    console.log("server on in http://localhost:3000");
+    
+})
