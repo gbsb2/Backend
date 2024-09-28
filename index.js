889ff0c658e47f9b0050785b83fde6ca3a5e8770
inwoo <inwooha00@naver.com>
2024-09-28 16:21:31 +0900

    Mod: add monggo connect code

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const app = express()
-
 // router import
+
+app.use(express.json());
+app.use(cors());
+
+// MongoDB 연결
+mongoose.connect('mongodb://127.0.0.1:27017/youtube', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // use router
 app.use()
