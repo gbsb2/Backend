0979d1d792193c0e9c21b212dd3569f75d8c145c
20223096 <won10201@kookmin.ac.kr>
2024-09-28 20:49:40 +0900

    commit

--- controller/quizController.js
+++ controller/quizController.js
@@ -1,58 +1,69 @@
-const express = require('express')
-const axios = require('axios')
-router.use(express.json())
-
-const {body, param, validationResult} = require('express-validator')
+const axios = require('axios');
+const { body, param, validationResult } = require('express-validator');
 const Quiz = require('../schema/quiz');
 
+const validate = (req, res, next) => {
+    const err = validationResult(req);
+    if (err.isEmpty()) {
+        return next();
+    } else {
+        return res.status(400).json(err.array());
+    }
+};
 
-
+// 퀴즈 조회
 exports.getQuiz = async (req, res) => {
-    const {id} = req.params;
-        try {
-            const quiz = await Quiz.findById(id);
-            if (quiz) {
-                const wordList = quiz.question.split(' ')
-                res.status(200).json({
-                    words : wordList
-                })
-            } else {
-                res.status(404).json({ message : "잘못된 요청입니다."})
-            }
-        } catch (err) {
-            res.status(500).json({message : "잘못된 요청입니다."})
-        }
-    }
+    const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({message: "유효하지 않은 id"})
+    }
+    try {
+        const quiz = await Quiz.findById(mongoose.Types.ObjectId(id));
+        if (quiz) {
+            const wordList = quiz.question.split(' ');
+            res.status(200).json({ words: wordList });
+        } else {
+            res.status(404).json({ message: "잘못된 요청입니다." });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "서버 오류입니다.", error: err });
+    }
+};
+
+// 퀴즈 제출
 exports.submitQuiz = async (req, res) => {
-        const {answer} = req.body
-        const {id} = req.params
-        try {
-            const quiz = await Quiz.findById(id)
-
-            if(!quiz) {
-                return res.status(404).json({ message : "퀴즈가 없습니다."})
-            }
-            if (quiz.answer == answer) {
-                try {
-                    const fruitResponse = await axios.get('./tree/fruit')
-                    res.status(200).json({
-                        message : "정답입니다",
-                        fruitData : fruitResponse.data
-                    })
-                } catch (error) {
-                    res.status(500).json({
-                        message : "정답이지만 fruit api요청 중 오류 발생",
-                        error : error.message
-                    })
-                }
-            } else {
-                res.status(200).json({
-                    message : "틀렸습니다.",
-                    correctAnswer : quiz.answer
-                })
-            }
-        } catch (err) {
-            res.status(500).json({message : "서버오류", error : err})
-        }
+    const { answer } = req.body;
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({message: "유효하지 않은 id"})
     }
+
+    try {
+        const quiz = await Quiz.findById(mongoose.Types.ObjectId(id));
+        if (!quiz) {
+            return res.status(404).json({ message: "퀴즈가 없습니다." });
+        }
+        if (quiz.answer == answer) {
+            try {
+                const fruitResponse = await axios.get('http://your-api-url.com/tree/fruit'); // 절대 URL로 변경
+                res.status(200).json({
+                    message: "정답입니다.",
+                    fruitData: fruitResponse.data
+                });
+            } catch (error) {
+                res.status(500).json({
+                    message: "정답이지만 fruit API 요청 중 오류 발생.",
+                    error: error.message
+                });
+            }
+        } else {
+            res.status(200).json({
+                message: "틀렸습니다.",
+                correctAnswer: quiz.answer
+            });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "서버 오류입니다.", error: err });
+    }
+};
