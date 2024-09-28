04792a0332358dd070d47a11607c3e9a63990276
20223096 <won10201@kookmin.ac.kr>
2024-09-28 20:51:09 +0900

    commit

--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -6,8 +6,8 @@ const { authenticateJWT } = require("../middlewares/auth");
 router.use(express.json())
 //로그인API 호출
 
-router.get("/quiz/:id", quizController.getQuiz)
+router.get("/:id", quizController.getQuiz)
 //회원가입API 호출
-router.post("/quiz/:id", quizController.submitQuiz);
+router.post("/:id", quizController.submitQuiz);
 
 module.exports = router;
