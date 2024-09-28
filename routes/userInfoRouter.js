62ec9f4f77d080aff716665ef45ce4df8546ab2b
20223096 <won10201@kookmin.ac.kr>
2024-09-28 23:11:35 +0900

    c

--- routes/userInfoRouter.js
+++ routes/userInfoRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userInfoController = require("../controller/userInfoController");
 const { authenticateJWT } = require("../middlewares/auth");
 
-
+router.use(express.json())
 //로그인API 호출
-router.post("/", userInfoController.info);
+router.post("/", authenticateJWT, userInfoController.info);
 
 module.exports = router;
