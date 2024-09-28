431a9652cd40e9d8ce5df1213fa1646be3434145
20223096 <won10201@kookmin.ac.kr>
2024-09-28 19:38:41 +0900

    commie

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,5 +5,5 @@ const userController = require("../controller/userController");
 
 //로그인API 호출
 router.post("/login", userController.login);
-
+router.post("/quiz", quizController.quiz);
 module.exports = router;
